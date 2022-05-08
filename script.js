47b52b1834b6ac34a7d2b537883f8f6f3f18aa3b
Robert <51803593+rotyvk7@users.noreply.github.com>
2022-05-08 18:34:11 -0300

    atualização do projeto

--- script.js
+++ script.js
@@ -6,6 +6,48 @@ onScroll() //Aqui estamos chamando a função para executar apenas uma única ve
 function onScroll() {
  showNavOnScroll()
  showBackToTopButtonOnScroll()
+
+ activateMenuAtCurrentSection(home)
+ activateMenuAtCurrentSection(services)
+ activateMenuAtCurrentSection(about)
+ activateMenuAtCurrentSection(contact)
+
+}
+
+function activateMenuAtCurrentSection(section) {
+  //linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  //verificar se a sessão passou da linha
+  //quais dados vou precisar?
+
+  //o topo da seção
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  //o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedLine = targetLine >= sectionTop
+
+  //verificar se a base está abaixo da linha alvo
+  //quais dados ovu precisar?
+
+  //a seção termina onde?
+  const sectionEnsdAt = sectionTop + sectionHeight
+
+  //o final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEnsdAt <= targetLine
+
+  //limites da seção
+  const sectionBoundaries =
+ sectionTopReachOrPassedLine &&
+  !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+  menuElement.classList.remove('active')
+  if(sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {//showNav faz a verificação se é necessário manter o menu fixo para o usuário durante o scroll ou se ele se encontra no início.
